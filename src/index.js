ea6131bdfd808af550d5ef30006992f3f613e75e
kadjialan <alankadji7@gmail.com>
2023-01-08 23:00:35 +0100

    modification

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import ui from './ui';
 import data from './data';
 import api from './api';
 
-/* ui.renderPage();
-api.getPopularMovies().then((movies) => {
-    data.setMovies(movies.meals);
-    ui.renderMovies(data.getMovies());
-}); */
 
 function updateMovies(movies) {
   data.setMovies(movies);
